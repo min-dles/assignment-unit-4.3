0fe21dedbdc68ae6d82f6540d8c019ebfc060593
mandi andrade <mandi.andrade20@gmail.com>
2023-03-19 11:39:34 -0500

    made variable & first two functions

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,27 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = [];
+
+// FIRST FUNCTION
+function addItem(item) { 
+    basket.push(item);
+    return true;
+}
+// tests:
+console.log(addItem('strawberries'));
+console.log(addItem('bread'));
+console.log(addItem('juice'));
+
+// SECOND FUNCTION
+function listItems() { 
+    for(item in basket){
+        console.log('This was added to your cart:', basket[item]);
+    }
+    return basket;
+}
+// test:
+console.log(listItems());
+
+// THIRD FUNCTION
